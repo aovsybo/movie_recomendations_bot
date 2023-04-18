bf9298ad19f6eb1b6789e8dfe0e41e674d672c19
matgr <matgr2n@gmail.com>
2023-04-18 21:08:44 +0300

    move settings from text_analysis to config

--- config.py
+++ config.py
@@ -13,18 +13,17 @@ class Settings(BaseSettings):
     MAX_KP_RATING = 10
     MOVIE_SEARCH_LIMIT = 10
     PREV_MOVIE_FILTERS = dict()
+    GENRE_NAMES = ['аниме', 'биография', 'боевик', 'вестерн', 'военный', 'детектив', 'детский', 'документальный',
+                   'драма', 'история', 'комедия', 'криминал', 'мелодрама', 'музыка', 'мультфильм', 'мюзикл',
+                   'приключения', 'семейный', 'спорт', 'ток-шоу', 'триллер', 'ужасы', 'фантастика', 'фэнтези']
+    RATING_NAMES = ["оценка", "рейтинг"]
+    YEAR_NAMES = ["год"]
 
     def __init__(self):
         load_dotenv(find_dotenv())
         TG_API_TOKEN = os.environ.get("TG_API_TOKEN")
         KP_API_TOKEN = os.environ.get("KP_API_TOKEN")
         super().__init__()
-
-    def get_movie_filters(self):
-        return getattr(self, "_prev_movie_filters")
-
-    def set_movie_filters(self, movie_filters):
-        setattr(self, "_prev_movie_filters", movie_filters)
 
 
 settings = Settings()
--- services/text_analysis.py
+++ services/text_analysis.py
@@ -2,27 +2,18 @@ import spacy
 from config import settings
 
 
-genre_names = ['аниме', 'биография', 'боевик', 'вестерн', 'военный', 'детектив', 'детский', 'для взрослых',
-               'документальный', 'драма', 'игра', 'история', 'комедия', 'концерт', 'короткометражка',
-               'криминал', 'мелодрама', 'музыка', 'мультфильм', 'мюзикл', 'новости', 'приключения',
-               'реальное ТВ', 'семейный', 'спорт', 'ток-шоу', 'триллер', 'ужасы', 'фантастика', 'фильм-нуар',
-               'фэнтези', 'церемония']
-rating_names = ["оценка", "рейтинг"]
-year_names = ["год"]
-
-
 def text_analyse(text: str):
     filters = dict()
     nlp = spacy.load("ru_core_news_sm")
     doc = nlp(text)
     for token in doc:
-        if token.lemma_ in genre_names:
+        if token.lemma_ in settings.GENRE_NAMES:
             filters["genres.name"] = [token.lemma_]
-        elif token.pos_ == "NOUN" and token.lemma_ in year_names:
+        elif token.pos_ == "NOUN" and token.lemma_ in settings.YEAR_NAMES:
             prev_token = token.nbor(-1)
             if prev_token.pos_ == "NUM" or prev_token.pos_ == "ADJ":
                 filters["year"] = f"{prev_token.text}-{settings.CURRENT_YEAR}"
-        elif token.pos_ == "NOUN" and token.lemma_ in rating_names:
+        elif token.pos_ == "NOUN" and token.lemma_ in settings.RATING_NAMES:
             next_token = token.nbor(1)
             if next_token.pos_ == "NUM":
                 filters["rating.kp"] = f"{round(float(next_token.text), 0)}-{settings.MAX_KP_RATING}"
