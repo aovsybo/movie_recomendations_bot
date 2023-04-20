31114a913dc0e71cce13d24368d528f1a5305a12
matgr <matgr2n@gmail.com>
2023-04-20 22:57:10 +0300

    decomposed text_analyse func

--- services/text_analysis.py
+++ services/text_analysis.py
@@ -7,14 +7,28 @@ def text_analyse(text: str):
     nlp = spacy.load("ru_core_news_sm")
     doc = nlp(text)
     for token in doc:
-        if token.lemma_ in settings.GENRE_NAMES:
+        if is_lemma_genre(token):
             filters["genres.name"] = [token.lemma_]
-        elif token.pos_ == "NOUN" and token.lemma_ in settings.YEAR_NAMES:
-            prev_token = token.nbor(-1)
-            if prev_token.pos_ == "NUM" or prev_token.pos_ == "ADJ":
-                filters["year"] = f"{prev_token.text}-{settings.CURRENT_YEAR}"
-        elif token.pos_ == "NOUN" and token.lemma_ in settings.RATING_NAMES:
-            next_token = token.nbor(1)
-            if next_token.pos_ == "NUM":
-                filters["rating.kp"] = f"{round(float(next_token.text), 0)}-{settings.MAX_KP_RATING}"
+        elif is_lemma_year(token):
+            filters["year"] = f"{token.nbor(-1).text}-{settings.CURRENT_YEAR}"
+        elif is_lemma_rating(token):
+            filters["rating.kp"] = f"{round(float(token.nbor(1).text), 0)}-{settings.MAX_KP_RATING}"
     return filters
+
+
+def is_lemma_genre(token):
+    return True if token.lemma_ in settings.GENRE_NAMES else False
+
+
+def is_lemma_year(token):
+    if token.pos_ == "NOUN" and token.lemma_ in settings.YEAR_NAMES:
+        if token.nbor(-1).pos_ == "NUM" or token.nbor(-1).pos_ == "ADJ":
+            return True
+    return False
+
+
+def is_lemma_rating(token):
+    if token.pos_ == "NOUN" and token.lemma_ in settings.RATING_NAMES:
+        if token.nbor(1).pos_ == "NUM":
+            return True
+    return False
